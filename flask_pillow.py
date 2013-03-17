0e6e01c4915489c4d6b57899c662ae0b84aa8943
Rich Harkins <rich.harkins@gmail.com>
2013-03-17 00:19:08 -0400

    Now will serialize objects with to_json

--- flask_pillow.py
+++ flask_pillow.py
@@ -248,12 +248,18 @@ def templatize(_entity, _template=None, _source=None, **_kw):
     else:
         raise ValueError('Either *_template or *_source must be set')
 
-def json_dumps(*_args, **_kw):
-    return json.dumps(*_args, default=lambda o: o.to_json(), **_kw)
-
 @Pillow.default_factory('json', 'text/json', 'application/json')
-def to_json(pillow, app, mimetypes, mimetype_override=None, **_kw):
+def to_json(pillow, app, mimetypes, mimetype_override=None,
+                    serialize_objects=None, **_kw):
     mimetypes = mimetype_override or app.config.get('json-types', mimetypes)
+    serialize_objects = (
+        app.config.get('json-serialize-objects', True)
+        if serialize_objects is None else serialize_objects)
+    if serialize_objects:
+        def json_dumps(*_args, **_kw):
+            return json.dumps(*_args, default=lambda o: o.to_json(), **_kw)
+    else:
+        json_dumps = json.dumps
     pillow.case(json_dumps, *mimetypes, obj='?')
 
 @Pillow.default_factory('yaml', 'text/yaml', 'application/yaml')
